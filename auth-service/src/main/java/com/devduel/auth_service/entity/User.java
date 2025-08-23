a7b288a57c96e9173bd03d2bb7ca263b9e98b95e
Adarxhhhh <katigiadarsh@gmail.com>
2025-08-23 10:21:23 -0500

    Initial commit

---
+++ auth-service/src/main/java/com/devduel/auth_service/entity/User.java
@@ -0,0 +1,43 @@
+package com.devduel.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Integer elo;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ auth-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    elo INTEGER NOT NULL DEFAULT 1000,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+    );
